68a7b2669297aa7ce11c4ae0e7017bb238ef0d6d
Konstancia Milova <konstanciamilova@Konstancias-MacBook-Pro.local>
2022-03-03 22:26:03 -0800

    basics

---
+++ src/Class1/Basics.java
@@ -0,0 +1,14 @@
+package Class1;
+
+public class Basics {
+
+        public static void main (String[] args) {
+
+            System.out.println("Hello, World!");
+
+
+        }
+        
+
+
+}
--- src/Class1/File1.java
+++ src/Class1/File1.java
@@ -1,4 +1,19 @@
 package Class1;
 
 public class File1 {
+
+    // this is a comment.
+    // note --> No code outside the curly-braces of class
+
+        /*
+         This is a
+         way to add
+         multi-line
+         comment
+        */
+
+    /**
+     * This is another way
+     * to add multi-line comment
+     */
 }
