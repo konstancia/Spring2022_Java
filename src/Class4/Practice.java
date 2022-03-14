c56f2b31a03878b26c03879044e76fe9374a402b
Konstancia Milova <konstanciamilova@Konstancias-MacBook-Pro.local>
2022-03-14 12:21:39 -0700

    homework week 4

---
+++ src/Class4/Practice.java
@@ -0,0 +1,15 @@
+package Class4;
+
+public class Practice {
+    public static void main(String[] args) {
+
+    String name1 = "kon";
+    String name2 = "Kon";
+
+    boolean compare = name1 != name2;
+    System.out.println("abc -->" +compare);
+
+
+
+    }
+}
